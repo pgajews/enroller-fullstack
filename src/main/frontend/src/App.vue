9c1a3219f52578a71cbd0019925534a0546f8ea7
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-06-13 16:14:04 +0200

    Added VueResource

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -13,6 +13,9 @@
     <div v-else>
       <button :class="registering ? 'button-outline' : ''" @click="registering = false">Zaloguj się</button>
       <button :class="registering ? '' : 'button-outline'" @click="registering = true">Rejestracja</button>
+
+      <div v-if="errorMessage" class='alert-warning'>{{ errorMessage }}</div>
+
       <login-form v-if="!registering" @login="login($event)"></login-form>
       <login-form v-if="registering" @login="register($event)"
                   button-label="Zarejestruj się"></login-form>
@@ -31,6 +34,7 @@
             return {
                 authenticatedUsername: "",
                 registering: false,
+                errorMessage: "",
             };
         },
         methods: {
@@ -41,7 +45,14 @@
                 this.authenticatedUsername = '';
             },
             register(user) {
-
+              this.$http.post('participants', user)
+              .then(response => {
+                this.errorMessage = ""
+                this.registering = false;
+              })
+              .catch(response => {
+                this.errorMessage = "Nie można zarejestrować użytkownika: użytkownik o nazwie \"" + user.login + "\" już istnieje"
+              });
             },
         }
     };
@@ -55,6 +66,13 @@
 
   .logo {
     vertical-align: middle;
+  }
+
+  .alert-warning {
+    border: 1px red dotted;
+    padding: 1px;
+    background: lightcoral;
+    border-radius: 2px;
   }
 </style>
 
