68626252a1ee823392889ca2e775c82c8a01facb
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-06-19 12:04:58 +0200

    Added rest methods for delete meeting, delete participant, add participant and create meeting

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,15 +1,15 @@
 package com.company.enroller.controllers;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 @RestController
@@ -27,5 +27,59 @@ public class MeetingRestController {
 
         Collection<Meeting> meetings = meetingService.getAll();
         return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> createMeeting(@Valid @RequestBody Meeting meeting) {
+        meetingService.add(meeting);
+        return new ResponseEntity<>(meeting, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{meetingId}", method = RequestMethod.POST)
+    public ResponseEntity<?> addParticipants(@PathVariable long meetingId, @RequestBody String participantToAdd) {
+        Participant participant = participantService.findByLogin(participantToAdd);
+            if (participant == null) {
+                return new ResponseEntity<>("Participant with login \"" + participantToAdd
+                    + "\" doesn't exist", HttpStatus.NOT_FOUND);
+            }
+        Meeting meeting = meetingService.findById(meetingId);
+        if (meeting == null) {
+            return new ResponseEntity<>("Meeting with id \"" + meetingId
+                + "\" doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        meetingService.addParticipant(meeting, participant);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/removeParticipant/{meetingId}", method = RequestMethod.POST)
+    public ResponseEntity<?> deleteParticipant(@PathVariable long meetingId, @RequestBody String participantToRemove) {
+        Meeting meeting = meetingService.findById(meetingId);
+        if (meeting == null) {
+            return new ResponseEntity<>("Meeting with id \"" + meetingId
+                + "\" doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        Participant participant = participantService.findByLogin(participantToRemove);
+        if (participant == null) {
+            return new ResponseEntity<>("Participant with login \"" + participantToRemove
+                + "\" doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        if (!meeting.getParticipants().contains(participant)) {
+            return new ResponseEntity<>("Participant with login \"" + participantToRemove
+                + "\" is not assigned to this meeting", HttpStatus.NOT_FOUND);
+        }
+
+        meetingService.removeParticipants(meeting, participant);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{meetingId}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteMeeting(@PathVariable("meetingId") long meetingId) {
+        Meeting meeting = meetingService.findById(meetingId);
+        if (meeting == null) {
+            return new ResponseEntity<>("Meeting with id \"" + meetingId
+                + "\" doesn't exist", HttpStatus.NOT_FOUND);
+        }
+        meetingService.delete(meeting);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
