dc4a7570908f48ac6333a388dffc0538d02d2cfc
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-06-19 12:08:10 +0200

    Added date field

--- src/main/frontend/src/meetings/NewMeetingForm.vue
+++ src/main/frontend/src/meetings/NewMeetingForm.vue
@@ -3,9 +3,11 @@
     <form @submit.prevent="addNewMeeting()" v-if="adding">
       <h3>Dodaj nowe spotkanie</h3>
       <label>Nazwa</label>
-      <input type="text" v-model="newMeeting.name">
+      <input type="text" v-model="newMeeting.title">
       <label>Opis</label>
       <textarea v-model="newMeeting.description"></textarea>
+      <label>Termin spotkania</label>
+      <input type="date" v-model="newMeeting.date">
       <button>Dodaj</button>
       <span class="error" v-if="error">Spotkanie musi mieć nazwę!</span>
     </form>
@@ -25,7 +27,7 @@
         methods: {
             addNewMeeting() {
                 this.error = false;
-                if (this.newMeeting.name) {
+                if (this.newMeeting.title) {
                     this.$emit('added', this.newMeeting);
                     this.newMeeting = {participants: []};
                     this.adding = false;
