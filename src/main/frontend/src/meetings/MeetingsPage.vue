a615c0dc9ca3cef55069faca8057b7d7ca183c26
Piotr Gajewski <extern.Piotr.Gajewski@itacsoftware.com>
2021-06-19 12:07:47 +0200

    Added rest endpoints

--- src/main/frontend/src/meetings/MeetingsPage.vue
+++ src/main/frontend/src/meetings/MeetingsPage.vue
@@ -43,17 +43,58 @@
         },
         methods: {
             addNewMeeting(meeting) {
-                this.meetings.push(meeting);
+                this.$http.post('meetings', meeting)
+                  .then(response => {
+                    this.avaliableMeetings.push(response.body);
+                  })
+                  .catch((error) => console.log(error));
             },
             addMeetingParticipant(meeting) {
-                meeting.participants.push(this.username);
+              this.$http.post(`meetings/${meeting.title}`, this.username)
+                  .then(response => {
+                    meeting.participants.push(this.username);
+                    this.avaliableMeetings.splice(this.meetings.indexOf(meeting), 1);
+                    this.meetings.push(meeting);
+                  })
+                  .catch(response => console.log(response.status));
             },
             removeMeetingParticipant(meeting) {
-                meeting.participants.splice(meeting.participants.indexOf(this.username), 1);
+              this.$http.post(`meetings/removeParticipant/${meeting.id}`, this.username)
+                  .then(response => {
+                    meeting.participants.splice(meeting.participants.indexOf(this.username), 1);
+                    this.meetings.splice(this.meetings.indexOf(meeting), 1);
+                    this.avaliableMeetings.push(meeting);
+                  })
+                  .catch(response => console.log(response.status));
             },
             deleteMeeting(meeting) {
-                this.meetings.splice(this.meetings.indexOf(meeting), 1);
-            }
-        }
+              this.$http.delete(`meetings/${meeting.id}`)
+                  .then(response => {
+                    this.avaliableMeetings.splice(this.avaliableMeetings.indexOf(meeting), 1);
+                  })
+                  .catch(response => console.log(response.status));
+            },
+            getMeetings() {
+              this.$http.get('meetings')
+                  .then(response => {
+                    for (let i=0; i < response.body.length; i++) {
+                      let participant = [];
+                      for (let j=0; j<response.body[i].participants.length; j++) {
+                        participant.push(response.body[i].participants[j].login);
+                      }
+                      response.body[i].participants = participant;
+                      if (response.body[i].participants.indexOf(this.username) < 0) {
+                        this.avaliableMeetings.push(response.body[i])
+                      } else {
+                        this.meetings.push(response.body[i])
+                      }
+                    }
+                  })
+                  .catch(response => console.log(response.status));
+            },
+        },
+      beforeMount() {
+          this.getMeetings();
+      }
     }
 </script>
